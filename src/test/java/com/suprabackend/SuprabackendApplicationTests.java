9dab4aaf71eaae4eab1f83af3835959743a3ff28
Avelardo <avelar.vignoto@gmail.com>
2026-05-02 10:32:10 -0300

    adicionando arquivos spring boot

---
+++ src/test/java/com/suprabackend/SuprabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suprabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuprabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
